d6cde1b284d0fe0d985556e9b9f0cdb8f848ccd8
xqd22 <1203550216@qq.com>
2020-10-30 17:40:10 +0800

    Update ss-panel-v3-mod-node-connect.sh

--- ss-panel-v3-mod-node-connect.sh
+++ ss-panel-v3-mod-node-connect.sh
@@ -11,9 +11,9 @@ Libtest(){
 	echo "$LIB_PING $LIB" >> ping.pl
 	libAddr=`sort -V ping.pl|sed -n '1p'|awk '{print $2}'`
 	if [ "$libAddr" == "$GIT" ];then
-		libAddr='https://raw.githubusercontent.com/lizhongnian/ss-panel-v3-mod-node-connect/master/libsodium-1.0.13.tar.gz'
+		libAddr='https://raw.githubusercontent.com/xqd22/ss-panel-v3-mod-node-connect/master/libsodium-1.0.13.tar.gz'
 	else
-		libAddr='https://download.libsodium.org/libsodium/releases/libsodium-1.0.13.tar.gz'
+		libAddr='https://download.libsodium.org/xqd22/releases/libsodium-1.0.13.tar.gz'
 	fi
 	rm -f ping.pl		
 }
@@ -98,7 +98,7 @@ install_centos_ssr(){
 	./configure && make -j2 && make install
 	echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
 	ldconfig
-	git clone -b manyuser https://github.com/lizhongnian/shadowsocks.git "/root/shadowsocks"
+	git clone -b manyuser https://github.com/xqd22/shadowsocks.git "/root/shadowsocks"
 	cd /root/shadowsocks
 	chkconfig supervisord on
 	#第一次安装
@@ -111,7 +111,7 @@ install_centos_ssr(){
 	#第三次检测是否成功
 	if [ -z "`python -c 'import requests;print(requests)'`" ]; then
 		mkdir python && cd python
-		git clone https://github.com/lizhongnian/urllib3.git && cd urllib3
+		git clone https://github.com/xqd22/urllib3.git && cd urllib3
 		python setup.py install && cd ..
 		git clone https://github.com/nakagami/CyMySQL.git && cd CyMySQL
 		python setup.py install && cd ..
@@ -145,7 +145,7 @@ install_ubuntu_ssr(){
 	apt-get install python-pip git -y
 	pip install cymysql
 	cd /root
-	git clone -b master https://github.com/lizhongnian/shadowsocks.git "/root/shadowsocks"
+	git clone -b master https://github.com/xqd22/shadowsocks.git "/root/shadowsocks"
 	cd shadowsocks
 	pip install -r requirements.txt
 	chmod +x *.sh
@@ -158,7 +158,7 @@ install_node(){
 	echo
 	echo "#########################################################################"
 	echo "# One click Install Shadowsocks-Python-Manyuser                         "
-	echo "# Github: https://github.com/lizhongnian/ss-panel-v3-mod-node-connect   "
+	echo "# Github: https://github.com/xqd22/ss-panel-v3-mod-node-connect   "
 	echo "# Author: 7colorblog                                                    "
 	echo "# blog: https://www.7colorblog.com                                      "	
 	echo "#########################################################################"
@@ -210,7 +210,7 @@ install_node(){
 	# 启用supervisord
 	supervisorctl shutdown
 	#某些机器没有echo_supervisord_conf 
-	wget -N -P  /etc/ --no-check-certificate  https://raw.githubusercontent.com/lizhongnian/ss-panel-v3-mod-node-connect/master/supervisord.conf
+	wget -N -P  /etc/ --no-check-certificate  https://raw.githubusercontent.com/xqd22/ss-panel-v3-mod-node-connect/master/supervisord.conf
 	supervisord
 	#iptables
 	iptables -F
@@ -224,7 +224,7 @@ install_node(){
 	chmod +x /etc/rc.d/rc.local
 	echo "#########################################################################"
 	echo "# 安装完成，节点即将重启使配置生效                                      "
-	echo "# Github: https://github.com/lizhongnian/ss-panel-v3-mod-node-connect   "
+	echo "# Github: https://github.com/xqd22/ss-panel-v3-mod-node-connect   "
 	echo "# Author: 7colorblog                                                    "
 	echo "# blog: https://www.7colorblog.com                                      "	
 	echo "#########################################################################"
@@ -235,7 +235,7 @@ install_node_db(){
 	echo
 	echo "#########################################################################"
 	echo "# One click Install Shadowsocks-Python-Manyuser                         "
-	echo "# Github: https://github.com/lizhongnian/ss-panel-v3-mod-node-connect   "
+	echo "# Github: https://github.com/xqd22/ss-panel-v3-mod-node-connect   "
 	echo "# Author: 7colorblog                                                    "
 	echo "# blog: https://www.7colorblog.com                                      "	
 	echo "#########################################################################"
@@ -294,7 +294,7 @@ install_node_db(){
 	# 启用supervisord
 	supervisorctl shutdown
 	#某些机器没有echo_supervisord_conf 
-	wget -N -P  /etc/ --no-check-certificate  https://raw.githubusercontent.com/lizhongnian/ss-panel-v3-mod-node-connect/master/supervisord.conf
+	wget -N -P  /etc/ --no-check-certificate  https://raw.githubusercontent.com/xqd22/ss-panel-v3-mod-node-connect/master/supervisord.conf
 	supervisord
 	#iptables
 	iptables -F
@@ -308,7 +308,7 @@ install_node_db(){
 	chmod +x /etc/rc.d/rc.local
 	echo "#########################################################################"
 	echo "# 安装完成，节点即将重启使配置生效                                      "
-	echo "# Github: https://github.com/lizhongnian/ss-panel-v3-mod-node-connect   "
+	echo "# Github: https://github.com/xqd22/ss-panel-v3-mod-node-connect   "
 	echo "# Author: 7colorblog                                                    "
 	echo "# blog: https://www.7colorblog.com                                      "
 	echo "#########################################################################"
@@ -317,7 +317,7 @@ install_node_db(){
 echo
 echo "########################################################################"
 echo "# ss-panel-v3-mod后端对接一键脚本                     				 "
-echo "# Github: https://github.com/lizhongnian/ss-panel-v3-mod-node-connect  "
+echo "# Github: https://github.com/xqd22/ss-panel-v3-mod-node-connect  "
 echo "# Author: 7colorblog                                                   "
 echo "# blog: https://www.7colorblog.com                                     "
 echo "# 请输入1或2选择对接方式                                               "
